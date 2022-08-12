2640050c98ef14533604a90404b259e85780a56d
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-11 20:20:05 -0700

    Practiced lession examples

---
+++ Java Script 11-08-2022/Interactive statements.js
@@ -0,0 +1,19 @@
+//1,2,3,4....100
+//initilization i=1; condition i<=100; incre/decre value i=i+1
+for(let i=2 ; i<=100 ;i=i+2){
+    console.log(i)
+}
+// Decremaental from 50 ,48, 46 ...2
+//initialization i=50; condition i>=2 ; incre/decer i=i-2
+for(let i=50; i>=2 ; i=i-2){
+    console.log(i)
+}
+
+//Create tables using for loop
+//5*1=5
+//5*2=10
+//take changing value and built around it
+//initilization i=1 : condition i<=10; incre/decre i=i+1;
+for(let d=1 ; d<=10 ; d=d+1){
+    console.log('5*',d,'=',5*d )
+}
---
+++ Java Script 11-08-2022/Tryout 2.js
@@ -0,0 +1,31 @@
+var salary=40000;
+if (salary<50000){
+    console.log("Eligiblefor all") //Conditional statement By If alone
+}
+
+if (1){
+    console.log("100")
+}
+else{
+    console.log('zero')
+}
+
+//Conditional statement By IF /else
+
+if(salary>30000){
+    console.log ('Eligible for all')
+}
+else{
+    console.log('Not eligible ')
+}
+
+if(salary % 7==0){
+    console.log ('divisible')
+}
+else{
+    console.log('not divisible')
+}
+
+//Ternary Operations
+
+salary % 10==0 ? console.log('even number salary') : console.log('Odd  salary')
