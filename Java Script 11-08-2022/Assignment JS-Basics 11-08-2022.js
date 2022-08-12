95db98a1d653fb5f02a225208fa53ea798590e70
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-12 02:00:50 -0700

    Assignments 11-08-2022

---
+++ Java Script 11-08-2022/Assignment JS-Basics 11-08-2022.js
@@ -0,0 +1,36 @@
+let a=310
+let b=200
+let c=230
+
+
+//Assignment No : 1
+if(a%7==0){
+    console.log('Divisible')
+}
+else{
+    console.log('Not Divisible')
+}
+
+//Assignment No : 2
+if(a*3==450){
+    console.log('Multiple of 3')
+}
+else{
+    console.log('Not Multiple of 3')
+}
+
+//Assignment No : 3 (Ternary Operation)
+a>=1 ? console.log('Positive number'):console.log('Negetive number')
+
+//Assignment No : 5 (Ternary Operation)
+
+a= 100<=a<=999 ?console.log('yes this is 3 digit number'):console.log('No this is not a 3 digit number')
+
+//Assignment No : 5  (Need clarity about this why not get even answer while using a value)
+b%2==0 ? console.log ("Yes this is a even number"):console.log ("No this is not a even number")
+
+//Assignment No : 6  (Big number)
+b<a ?console.log('biggest number is',a):console.log('biggest number is',b)
+
+//Assignment No : 7  (Small number)
+b<a ?console.log('Smallest number is',b):console.log('smallest number is',a)
---
+++ Java Script 11-08-2022/Assignment Js-basics 2.js
@@ -0,0 +1,10 @@
+let a=310
+let b=200
+let c=230
+
+
+//Assignment No : 6  (Big number)
+b<a ?console.log('biggest number is',a):console.log('biggest number is',b)
+
+//Assignment No : 7  (Small number)
+b<a ?console.log('Smallest number is',b):console.log('smallest number is',a)
