bcf884d268af374eb1f21e844956c4283ffde780
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-12 03:06:46 -0700

    Assignment UPDATE"

--- Java Script 11-08-2022/Assignment Js-basics 2.js
+++ Java Script 11-08-2022/Assignment Js-basics 2.js
@@ -1,6 +1,6 @@
 let a=310
 let b=200
-let c=230
+let c=630
 
 
 //Assignment No : 6  (Big number)
@@ -8,3 +8,33 @@ b<a ?console.log('biggest number is',a):console.log('biggest number is',b)
 
 //Assignment No : 7  (Small number)
 b<a ?console.log('Smallest number is',b):console.log('smallest number is',a)
+
+//Assignment No : 8   (Bigest number)
+var x= Math.max(a,b,c)
+console.log("Biggest number",x)
+
+//Assignment no : 8 (Biggist number)
+if (a>=b && a>=c) {
+    console.log('biggest',a)
+}
+else if(b>=a && b>=c) {
+    console.log('biggest',b)
+} 
+else {
+    console.log('Biggist',c)
+}
+
+//Assignment No : 9   (Bigest number)
+var x= Math.min(a,b,c)
+console.log("Smallest number",x)
+
+//Assignment no : 9 (Biggist number)
+if (a<=b && a<=c) {
+    console.log('Smallest',a)
+}
+else if(b<=a && b<=c) {
+    console.log('Smallest',b)
+} 
+else {
+    console.log('Smallest',c)
+}
