ccd5eb465a614131bd89355ec20cbde63b75e0c3
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-10-06 14:30:40 +0530

    Interview Practice

--- Interview practice/string comparision challange/stringcomp.js
+++ Interview practice/string comparision challange/stringcomp.js
@@ -1,19 +1,45 @@
-let a="arm"
-let b="aramasdas"
+let a = "aak"
+let b = "armasdas"
 
-let string1=a.split('').sort().join('')
+let string1 = a.split('').sort()
+let string2 = b.split('').sort()
+
+console.log(string1);
+console.log(string2);
+
+//Programme for each letter Itteration and Comparision
+for (str1 of string1) {
+     string2.includes(str1) ? console.log(true) : console.log(false);
+    
+}
+
+// This is for word inside the string or not
+
+/* string2.includes(string1) ? true : false
+console.log(`The word "${string1}" ${string2.includes(string1) ? 'is' : 'is not'} in the sentence`); */
+
+
+
+
+
+// DRAFT PROGRAMES Don't try
+
+
+
+/* let string1=a.split('').sort().join('')
 let string2=b.split('').sort().join('')
 
-for (i=0; i<=string1.length-1 || i>=string1.length-1  ;i++){
+
+for (i=0; i<=(string1.length-1 || string2.length-1 );i++){
 
 if(string2[i]===string1[i]){
     console.log('True');
 }
-else{
+else {
     console.log('False');
 }
 }
-
+ */
 /* for (i=0;string2[i]===string1[i] ;i++){
 
     if(i !=""){
@@ -23,8 +49,8 @@ else{
         console.log('False');
     }
     } */
-    
-    
-   
+
+
+
 
 /* console.log(a[0]); */
