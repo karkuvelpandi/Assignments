dea3f7e66d09a764dcab1526bd96b9205046d295
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-11-08 16:39:51 +0530

    ok

--- Interview practice/100Examples/Ex11_PossiblePair.js
+++ Interview practice/100Examples/Ex11_PossiblePair.js
@@ -1,19 +1,30 @@
 
 
 let array = [1, 2, 3, 4, 5, 6]
-let array1= [1, 2, 3, 4, 5, 6]
+
 let n = 8
 let b="";
 let c=""; 
 
+//using for of loop
 for (let a of array){
-   for(let ax of array1){
+   for(let x of array){
     b=a
-    c=ax
+    c=x
    if(b+c===8) {
     console.log(b,c);
    }
    }
-    
-   
-}
+    }
+
+//using for loop
+/* for(let i=0; i<=array.length-1;i++){
+    for(let j=0;j<=array.length-1;j++){
+        b=array[i]
+        c=array[j]
+        if(b+c===8){
+        console.log(b,c);
+        }
+    }
+
+} */
