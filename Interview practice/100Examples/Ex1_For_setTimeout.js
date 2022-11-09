03aacc7aeec765d558ef5bfcaeda9f230d0d5d3a
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-11-09 18:04:10 +0530

    ok

--- Interview practice/100Examples/Ex1_For_setTimeout.js
+++ Interview practice/100Examples/Ex1_For_setTimeout.js
@@ -1,18 +1,20 @@
 
 //print1234Using
 
-
-/* for (let i=1;i<=5;i++){
-    setTimeout(()=>{
-        console.log(i);
-    },i*2000)
-} */
-
-let i=1;
+//working programe but 
+ /*  let i=1
 while(i<=5){
     setTimeout(()=>{
+        console.log(i)
+        i++
+    },2000)
+ 
+}   */
+
+
+
+  for (let i=1;i<=5;i++){
+    setTimeout(()=>{
         console.log(i);
     },i*2000)
-    
-}
-
+}   
