17d548730992f7d23862ea68bc098a2a0bbba440
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-27 21:13:21 +0530

    'ok'

---
+++ Interview practice/100Examples/Ex_21_SortwithoutInbuilt.js
@@ -0,0 +1,13 @@
+let arr=[1,11,45,6,4,8,99,75]
+let arr1=[1,11,45,6,4,8,99,75]
+
+let result=[]
+for (a of arr){
+   for(b of arr1){
+    if(a<(b+1)){
+        
+    }
+   
+   }
+}
+console.log(result);
---
+++ Interview practice/100Examples/Ex_22_AmstrongNumber.js
@@ -0,0 +1,9 @@
+// // 153 = 1*1*1 + 5*5*5 + 3*3*3
+
+// let number=153
+// let a=number.toString().slice(0,1)
+// let b=number.toString().slice(1,2)
+// let c=number.toString().slice(2,3)
+
+// console.log((number.toString().length-1));
+// // console.log(a,b,c);
