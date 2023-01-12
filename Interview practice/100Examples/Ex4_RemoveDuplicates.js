fbc4e09024f96e4ed478cf38003d4cf39dd37f6a
Mohd-Imad <mohdimd123@gmail.com>
2023-01-12 16:40:26 +0530

    ok

--- Interview practice/100Examples/Ex4_RemoveDuplicates.js
+++ Interview practice/100Examples/Ex4_RemoveDuplicates.js
@@ -1,13 +1,39 @@
-var arr=[1,2,3,4,5,2,4]
+/* var arr = [1, 2, 3, 4, 5, 2, 4]
+console.log(arr);
 
 console.log(arr.length);
 
-function removeDuplicates(){
-     return arr.filter((item,index)=>arr.indexOf(item)===index )
-   
+function removeDuplicates() {
+     return arr.filter((item, index) => arr.indexOf(item) === index)
+
 }
 console.log(removeDuplicates(arr))
 console.log(removeDuplicates(arr).length)
 //default method for removing duplicate
-let newArr=[...new Set(arr)]
-console.log(newArr) 
+let newArr = [...new Set(arr)]
+console.log(newArr) */
+
+
+//Alternate method
+
+// let arr = [1,2,3,4,3,2,5]
+let arr = ['a','b','a']
+
+console.log(arr);
+console.log(arr.length);
+
+ function removeDuplicates(arr){
+     let newArr = []
+    for(i=0;i<arr.length;i++){
+     if(newArr.indexOf(arr[i])===-1){
+          newArr.push(arr[i])
+     }
+    }
+    return newArr
+}
+console.log(removeDuplicates(arr));
+
+
+
+
+
