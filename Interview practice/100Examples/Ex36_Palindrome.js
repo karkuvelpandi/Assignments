f787d2cbc03e0383c08f4456ae79bcef4e3fc4da
Mohd-Imad <mohdimd123@gmail.com>
2023-02-16 17:00:50 +0530

    ok

--- Interview practice/100Examples/Ex36_Palindrome.js
+++ Interview practice/100Examples/Ex36_Palindrome.js
@@ -8,29 +8,49 @@ if (string === result1) {
   console.log("No its not a palindrome");
 }
 
-// without inbuild method
-// let arr = [];
-// for (a of string) {
-//   arr.unshift(a);
-  
-// }
-// console.log(arr);
 
 function palindrome(str) {
-    // Step 1. The first part is the same as earlier
-    var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
-    str = str.toLowerCase().replace(re, '');
-   
-    // Step 2. Create the FOR loop
-    var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
-    console.log(len);
-    for (var i = 0; i < len; i++) {
-      if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
-          return false; // When the characters don't match anymore, false is returned and we exit the FOR loop
-      }
+  // Step 1. The first part is the same as earlier
+  var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+  str = str.toLowerCase().replace(re, '');
+
+  // Step 2. Create the FOR loop
+  var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+  console.log(len);
+  for (var i = 0; i < len; i++) {
+    if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
+      return false; // When the characters don't match anymore, false is returned and we exit the FOR loop
     }
-    return true; // Both parts are strictly equal, it returns true => The string is a palindrome
-   }
-   
-   let result=palindrome("A man, a plan, a canal. Panama");
-   console.log(result);
+  }
+  return true; // Both parts are strictly equal, it returns true => The string is a palindrome
+}
+
+let result = palindrome("A man, a plan, a canal. Panama");
+console.log(result);
+
+//  -----------------------------------------------------------------------------------------------------------------------
+
+
+let str = "madam"
+
+let arr1 = []
+for (a of str) {
+  arr1.unshift(a)
+}
+console.log(arr1);
+
+let arr2 = []
+for (b of str) {
+  arr2.push(b)
+}
+console.log(arr2);
+
+for (let i = 0; i < str.length - 1; i++) {
+  if (arr1[i] === arr2[i]) {
+    return console.log('yes');
+  }
+  else {
+    return console.log('no');
+
+  }
+}
